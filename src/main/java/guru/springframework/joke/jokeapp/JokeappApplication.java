0a9ff0e09e2fc069fe413a9c7da2fc631eb62f8c
Tom <thomasbaratta@yahoo.com>
2019-03-07 07:03:19 -0500

    Git Test

---
+++ src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
@@ -0,0 +1,17 @@
+package guru.springframework.joke.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Testing Git Connection
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+
+
+}
+
