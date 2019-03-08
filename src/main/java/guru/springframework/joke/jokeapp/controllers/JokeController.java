4a8f5fbaa2f14bae96758a04e0b6dd3d53dc0c62
Tom <thomasbaratta@yahoo.com>
2019-03-08 08:08:33 -0500

    Joke MVC with @Controller, Spring @Autowired, and @RequestMapping. Also Ctrl O from the Main Method brought in joke apps from our Maven Repository. Now onto Creating Thymleaf View functionality.

---
+++ src/main/java/guru/springframework/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package guru.springframework.joke.jokeapp.controllers;
+
+import guru.springframework.joke.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        //addAttribute method may be wrong. TRY ANOTHER METHOD WITH (s:) in place of (attributeName:)
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+
+    }
+}
